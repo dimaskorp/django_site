276eaffbefc92d2667e2391686bb7c14a40b27c7
dimaskorp <dima-skorp@mail.ru>
2022-11-11 11:18:51 +0400

    Редактирование и удаление записей

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -9,10 +9,25 @@ def news_home(request):
     return render(request, 'news/news_home.html', {'news': news})
 
 
-class NewsDetaleView(DetailView):
+class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/details_view.html'
     context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+    # fields = ['title', 'anons', 'full_text', 'date']
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news_delete.html'
+
 
 
 def create(request):
